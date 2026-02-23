5bfd0624b19ec52c1a81f34b0fd0c8eda2aa4af6
Nithish <nk0283@srmist.edu.in>
2026-02-23 09:49:41 +0530

    added hardcoded palindrome

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 public class palindromecheckerapp{
     public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");;
+        System.out.println("System initialized successfully");
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the string");
         String str = input.nextLine();
