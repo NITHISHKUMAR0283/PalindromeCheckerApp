0caa53b9bc0e260840f6dd1f407f449d621d8621
garima-jand <garimajand10@gmail.com>
2026-03-02 10:12:02 +0530

    created branch for uc9

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,65 +1,24 @@
 import java.util.Scanner;
 
-
-public class palindromecheckerapp{
-    public static void main(String[] args){
+public class palindromecheckerapp {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a string");
-        class Node {
-            char data;
-            Node next;
-            Node(char data){
-                this.data = data;
-                this.next=null;
-            }
-        }
-        Node head=null;
-        Node prev=null;
         String str = input.nextLine();
-        int size = str.length();
-        for ( char c :str.toCharArray()){
-            Node newNode = new Node(c);
-            if (head == null){
-                head = newNode;
-                prev = newNode;
-            }
-            else{
-                prev.next = newNode;
-                prev= prev.next;
-            }
-        }
-        int i =0;
-        Node fast=head;
-        Node slow =head;
-        while(fast!=null && fast.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
-        }
-        Node head2=slow.next;
-        slow.next=null;
-        Node next ;
-        Node pr=null;
-        Node cu = head2;
-        while (cu!=null){
-            next=cu.next;
-            cu.next=pr;
-            pr=cu;
-            cu=next;
-        }
-        boolean ispalindrome = true;
-        while (head!=null && head2!=null){
-            if (head.data!=head2.data){
-                ispalindrome=false;
-                break;
-            }
-            head = head.next;
-            head2=head2.next;
-        }
-        if (ispalindrome){
+        if (isPalindrome(str, 0, str.length() - 1)) {
             System.out.println("It is a palindrome");
-        }
-        else{
+        } else {
             System.out.println("It is not a palindrome");
         }
+    }
+
+    static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+        return isPalindrome(str, start + 1, end - 1);
     }
 }
